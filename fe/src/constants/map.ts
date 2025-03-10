dfc70b3befa1c059795c7200e7a01bd12a531352
minjeongHEO <hmjoung9770@gmail.com>
2025-03-10 14:03:37 +0900

    feat: 시간 기본 값 함수로 constant로 선언 #187

--- fe/src/constants/map.ts
+++ fe/src/constants/map.ts
@@ -1,4 +1,10 @@
-import { PositionType } from '@/types/map';
+import { PositionType, TimeRef } from '@/types/map';
 
 export const DEFAULT_MAP_POSITION: PositionType = [37.566418, 126.977943];
 export const DEFAULT_MAP_LEVEL = 3;
+
+export const createInitTimeRef = (): TimeRef => ({
+  start: { day: new Date(), time: '' },
+  end: { day: new Date(), time: '' },
+  total: '',
+});
